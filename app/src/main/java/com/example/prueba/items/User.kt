602a68398f8bb070f5ecdd1ba44908c08f2be062
Daniel Vargas <dvargasp@javeriana.edu.co>
2023-11-20 22:51:58 -0500

    old interface

---
+++ app/src/main/java/com/example/prueba/items/User.kt
@@ -0,0 +1,12 @@
+package com.example.prueba.items
+import java.io.Serializable
+
+class User(var nombre: String? = null,
+           var apellido: String? = null,
+           var email: String? = null,
+           var password: String? = null,
+           var activo: Boolean = false,
+           var imageUrl: String? = null,
+           var latitude : Double = 0.0,
+           var longitude : Double = 0.0
+): Serializable
