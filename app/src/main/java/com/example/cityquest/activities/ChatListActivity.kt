80b3571df758f9fc4ec8b9d48f9a3a741eaf559c
Daniel Vargas <dvargasp@javeriana.edu.co>
2023-11-22 06:59:06 -0500

    otro push sin force :<

--- app/src/main/java/com/example/cityquest/activities/ChatListActivity.kt
+++ app/src/main/java/com/example/cityquest/activities/ChatListActivity.kt
@@ -1,9 +1,17 @@
 package com.example.cityquest.activities
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import com.example.cityquest.R
 import com.example.cityquest.adapters.UserAdapter
 import com.example.cityquest.databinding.ActivityChatListBinding
 import com.example.cityquest.models.User
@@ -11,13 +19,17 @@ import com.google.firebase.Firebase
 import com.google.firebase.appcheck.appCheck
 import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
 import com.google.firebase.initialize
+import com.parse.ParseObject
 import com.parse.ParseQuery
 import com.parse.ParseUser
+import com.parse.livequery.ParseLiveQueryClient
+import com.parse.livequery.SubscriptionHandling
 
 class ChatListActivity : AppCompatActivity() {
     private lateinit var binding: ActivityChatListBinding
     private lateinit var userAdapter: UserAdapter
     private var userList = mutableListOf<User>()
+    private lateinit var liveQueryClient: ParseLiveQueryClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +40,7 @@ class ChatListActivity : AppCompatActivity() {
 
         setupListView()
         loadUsersFromParse()
+        initLiveQuery()
 
         Firebase.initialize(context = this)
         Firebase.appCheck.installAppCheckProviderFactory(
@@ -61,5 +74,57 @@ class ChatListActivity : AppCompatActivity() {
                 Toast.makeText(this, "Error al cargar usuarios: ${e.localizedMessage}", Toast.LENGTH_LONG).show()
             }
         }
+    }
+
+    private fun initLiveQuery() {
+        try {
+            liveQueryClient = ParseLiveQueryClient.Factory.getClient()
+            Log.d("ChatActivity", "LiveQuery Client iniciado correctamente")
+            subscribeToMessages()
+        } catch (e: Exception) {
+            Log.e("ChatActivity", "Error al iniciar LiveQuery: ${e.localizedMessage}")
+        }
+    }
+
+    private fun subscribeToMessages() {
+        val currentUser = ParseUser.getCurrentUser()
+        val query = ParseQuery.getQuery<ParseObject>("Mensaje")
+        query.whereEqualTo("id_receptor", currentUser.objectId)
+        //query.whereEqualTo("id_emisor", "idChatContrario")
+
+        val subscriptionHandling: SubscriptionHandling<ParseObject> = liveQueryClient.subscribe(query)
+
+        subscriptionHandling.handleEvent(SubscriptionHandling.Event.CREATE) { _, mensaje ->
+            runOnUiThread {
+                Log.d("ChatActivity", "Nuevo mensaje recibido a través de LiveQuery")
+                val contenidoMensaje = mensaje.getString("contenido") ?: ""
+                val emisor = mensaje.getString("id_emisor") ?: ""
+                sendNotification(this, this@ChatListActivity::class.java, contenidoMensaje, "¡Nuevo mensaje!")
+            }
+        }
+    }
+
+    private fun sendNotification(context: Context, targetActivity: Class<*>, message: String, title: String) {
+        val channelId = "cityquest"
+
+        val notificationManager = context.getSystemService(AppCompatActivity.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId, "Canal de notificaciones", NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val intent = Intent(context, targetActivity)
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
+
+        val builder = NotificationCompat.Builder(context, channelId)
+            .setSmallIcon(R.drawable.logo)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        notificationManager.notify(1, builder.build())
     }
 }
