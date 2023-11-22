c6395177d76ccb49f14cde5a942c0ba68416b6e0
Javi <javisdgv@gmail.com>
2023-11-22 02:23:44 -0500

    Fix message

--- app/src/main/java/com/example/cityquest/items/User.kt
+++ app/src/main/java/com/example/cityquest/items/User.kt
@@ -2,11 +2,11 @@ package com.example.cityquest.items
 import java.io.Serializable
 
 class User(var nombre: String? = null,
-           var apellido: String? = null,
+           var imageUrl: String? = null,
            var email: String? = null,
            var password: String? = null,
            var activo: Boolean = false,
-           var imageUrl: String? = null,
+           var apellido: String? = null,
            var latitude : Double = 0.0,
            var longitude : Double = 0.0
 ): Serializable
